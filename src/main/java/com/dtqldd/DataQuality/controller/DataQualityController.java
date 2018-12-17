0f6038cd68ca3fabdebd6f035a37900fdf3ee32d
INDRA\idomingos <idomingos@indracompany.com>
2018-12-17 13:59:27 -0200

    Atualizacao tela

--- src/main/java/com/dtqldd/DataQuality/controller/DataQualityController.java
+++ src/main/java/com/dtqldd/DataQuality/controller/DataQualityController.java
@@ -6,9 +6,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DataQualityController {
 
-		@RequestMapping("/")
-	    public String exemploP(){
-	        return "Parametrizacao";
+		@RequestMapping("/parammovel")
+	    public String ParamMovel(){
+	        return "paramMovel";
+	    }
+		
+		@RequestMapping("/paramfixa")
+	    public String ParamFixa(){
+	        return "paramFixa";
 	    }
 	
+		@RequestMapping("/")
+	    public String index(){
+	        return "index";
+	    }
+		
+		@RequestMapping("/quali")
+	    public String quali(){
+	        return "index_teste";
+	    }
+		
+		@RequestMapping("/qua")
+	    public String qual(){
+	        return "teste_index";
+	    }
 }
--- src/main/java/com/dtqldd/DataQuality/controller/GeraArquivo.java
+++ src/main/java/com/dtqldd/DataQuality/controller/GeraArquivo.java
@@ -3,11 +3,9 @@ package com.dtqldd.DataQuality.controller;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import net.schmizz.sshj.SSHClient;
 import net.schmizz.sshj.sftp.SFTPClient;
 import net.schmizz.sshj.transport.verification.PromiscuousVerifier;
@@ -15,88 +13,101 @@ import net.schmizz.sshj.transport.verification.PromiscuousVerifier;
 @Controller
 public class GeraArquivo {
 	
-	 @RequestMapping("Parametros")
-	    public String someMethod(@RequestParam ("banco") String banco,
-	    						 @RequestParam ("tabela") String tabela,
-	    						 @RequestParam("owner") String owner,
-	    						 @RequestParam("processo") String processo,
-	    						 @RequestParam("dataref") String dataref,
-	    						 @RequestParam("ciclo") String ciclo,
-	    						 @RequestParam("desvio") double desvio) throws IOException {
+	int i = 0;
+	
+	 @RequestMapping("ParametrosMovel")
+	    public String parametrosMovel(@RequestParam ("banco") String banco,
+	    						 	@RequestParam("tabela") String tabela,
+	    						 	@RequestParam("owner") String owner,
+	    						 	@RequestParam("processo") String processo,
+	    						 	@RequestParam("dataref") String dataref,
+	    						 	@RequestParam("desvio") double desvio,
+	    						 	@RequestParam("sla") int sla,
+	    						 	@RequestParam("agroup") String agrupador) throws IOException {
 
 	 SSHClient ssh = new SSHClient();
-	   // required if host is not in knownLocalHosts
 	   ssh.addHostKeyVerifier(new PromiscuousVerifier());
 
 	   try {
 	     ssh.connect("10.129.227.177", 22);
 	     ssh.authPassword("a5113588", "a5113588");
 
-	     if(dataref.equals("DT_MES_FTRA")) {
-     		FileWriter arq = new FileWriter("d:\\teste_iago.txt",true);
+     		FileWriter arq = new FileWriter("d:\\arqv_cadastro.txt",true);
      		
+     		i++;
+    		if(!agrupador.equals("")) {
      		PrintWriter gravarArq = new PrintWriter(arq);
-     	    gravarArq.println(banco+';'+tabela.toUpperCase()+';'+owner.toUpperCase()+';'+processo+";"+Double.toString(desvio/100)+";"+dataref+";"+ciclo);
+     		gravarArq.println(banco+';'+tabela.toUpperCase()+';'+owner.toUpperCase()+";"+processo+";"+Integer.toString(sla)+";"+Double.toString(desvio)+";"+dataref+";"+agrupador+';'+i);
      	    arq.close();
- 			
- 		}else {
-     		FileWriter arq = new FileWriter("d:\\teste_iago.txt",true);
-     		
-     		PrintWriter gravarArq = new PrintWriter(arq);
-     	    gravarArq.println(banco+';'+tabela.toUpperCase()+';'+owner.toUpperCase()+';'+processo+";"+Double.toString(desvio/100)+";"+dataref+";"+"");
-     	    arq.close();
- 			
- 		}
-	     SFTPClient sftp = ssh.newSFTPClient();
-	     sftp.put("d:\\teste_iago.txt",  "/dw/trf/DATA_QLDD_BI/");
+    		}
+    		if(agrupador.equals("")) {
+         		PrintWriter gravarArq = new PrintWriter(arq);
+         		gravarArq.println(banco+';'+tabela.toUpperCase()+';'+owner.toUpperCase()+";"+processo+";"+Integer.toString(sla)+";"+Double.toString(desvio)+";"+dataref+";"+"''"+';'+i);
+         	    arq.close();
+        		}
+    		
+    		
+	   SFTPClient sftp = ssh.newSFTPClient();
+	     sftp.put("d:\\arqv_cadastro.txt",  "/dw/trf/DATA_QLDD_BI/");
 	     ssh.disconnect();
 
 	   } catch( Exception e ) { 
-	     //TODO: this needs to be more robust than just catching all exceptions
 	     e.printStackTrace();
 	     return null;
 	   } 
   	
 
-	    	    return "redirect:/";
+	    	    return "redirect:/parammovel";
 	    	    
 	 }
 	 
 	 
-//	 @RequestMapping("Parametros")
-//	 public FileWriter putTempLocalInstance(  )
-//	 {
-//	   FileWriter tempFile = null;
-//	   SSHClient client = new SSHClient();
-//	   // required if host is not in knownLocalHosts
-//	   client.addHostKeyVerifier(new PromiscuousVerifier());
-//
-//	   try {
-//	     client.connect("10.129.227.177", 22);
-//	     client.authPassword("a5113588", "a5113588");
-//
-//	     tempFile = new FileWriter("d:\\tempFile.txt",true);
-//	     PrintWriter gravarArq = new PrintWriter(tempFile);
-//	     gravarArq.println("Testando SSH Server");
-//	     tempFile.close();
-//
-//	     SFTPClient sftp = client.newSFTPClient();
-//	     sftp.put("d:\\tempFile.txt",  "/dw/trf/DATA_QLDD_BI/");
-//	     client.disconnect();
-//
-//	   } catch( Exception e ) { 
-//	     //TODO: this needs to be more robust than just catching all exceptions
-//	     e.printStackTrace();
-//	     return null;
-//	   } 
-//	   
-//	   return tempFile;
-//	 }
-	  
-	  
+	 @RequestMapping("ParametrosFixa")
+	    public String parametrosFixa(@RequestParam ("banco") String banco,
+	    						     @RequestParam("tabela") String tabela,
+	    						     @RequestParam("owner") String owner,
+	    						     @RequestParam("processo") String processo,
+	    						     @RequestParam("dataref") String dataref,
+	    						     @RequestParam("desvio") double desvio,
+	    						     @RequestParam("sla") int sla,
+	    						     @RequestParam("agroup") String agrupador) throws IOException {
+
+	 SSHClient ssh = new SSHClient();
+	   ssh.addHostKeyVerifier(new PromiscuousVerifier());
+
+	   try {
+	     ssh.connect("10.129.227.177", 22);
+	     ssh.authPassword("a5113588", "a5113588");
+
+  		FileWriter arq = new FileWriter("d:\\arqv_cadastro.txt",true);
+  		
+  		i++;
+ 		if(!agrupador.equals("")) {
+  		PrintWriter gravarArq = new PrintWriter(arq);
+  		gravarArq.println(banco+';'+tabela.toUpperCase()+';'+owner.toUpperCase()+";"+processo+";"+Integer.toString(sla)+";"+Double.toString(desvio)+";"+dataref+";"+agrupador+';'+i);
+  	    arq.close();
+ 		}
+ 		if(agrupador.equals("")) {
+      		PrintWriter gravarArq = new PrintWriter(arq);
+      		gravarArq.println(banco+';'+tabela.toUpperCase()+';'+owner.toUpperCase()+";"+processo+";"+Integer.toString(sla)+";"+Double.toString(desvio)+";"+dataref+";"+"''"+';'+i);
+      	    arq.close();
+     		}
+ 		
+ 		
+	   SFTPClient sftp = ssh.newSFTPClient();
+	     sftp.put("d:\\arqv_cadastro.txt",  "/dw/trf/DATA_QLDD_BI/");
+	     ssh.disconnect();
+
+	   } catch( Exception e ) { 
+	     e.printStackTrace();
+	     return null;
+	   } 
+	
+
+	    	    return "redirect:/paramfixa";
+	    	    
+	 }
 	 
-		
-		
 }
 
 
